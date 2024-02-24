df61f8d29b6195f8b02b1c4e8c629d036be88772
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-02-23 22:38:43 -0900

    View blocks are added and styled

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, Lord!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer} >
+			<View style={styles.inputContainer} >
+				<TextInput style={styles.textIput} placeholder="Your course goal!" />
+				<Button title="Add Goal" />
+			</View>
+			<View style={styles.goalsContainer} >
+				<Text>List of goals...</Text>
+			</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: {
+		flex: 1,
+		paddingTop: 50,
+		paddingHorizontal: 16
+	},
+	inputContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		paddingBottom: 20,
+		borderBottomWidth: 1,
+		borderBottomColor: 'black'
+	},
+	textIput: {
+		width: '60%',
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 8
+	},
+	goalsContainer: {
+		flex: 5,
+		// justifyContent: 'center',
+		// alignItems: 'center'
+	}
 });
